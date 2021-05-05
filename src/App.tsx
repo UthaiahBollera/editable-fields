b76f9fc5748c2e309b32889fbe18983c78a20d18
Sanju Uthaiah Bollera <bollera.sanju@outlook.com>
2021-05-05 18:44:09 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./styles.css";
+interface Props {}
+
+const AddLabels: React.FC<Props> = () => {
+  return <div className="add-labels"></div>;
+};
+export default AddLabels;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+
+import AddLabels from "./App";
+
+const rootElement = document.getElementById("root");
+render(<AddLabels />, rootElement);
